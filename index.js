c7a7b53329b5facf21d1a13ac5c426aed31612da
keemewoon <keemewoon@gmail.com>
2024-02-15 23:38:39 +0900

    싸이월드 만들기

---
+++ index.js
@@ -0,0 +1,19 @@
+const menuHome = () => { 
+    document.getElementById("contentFrame").setAttribute("src", "home.html")
+    document.getElementById("menuHome").style = "color: black; background-color: white;"
+    document.getElementById("menuGame").style = "color: white; background-color: #298b5;"
+    document.getElementById("menuJukebox").style = "color: white; background-color: #298b5;"
+    
+}
+const menuJukebox = () => { 
+    document.getElementById("contentFrame").setAttribute("src", "jukebox.html")
+    document.getElementById("menuJukebox").style = "color: black; background-color: white;"
+    document.getElementById("menuGame").style = "color: white; background-color: #298b5;"
+    document.getElementById("menuHome").style = "color: white; background-color: #298b5;"
+}
+const menuGame = () => { 
+    document.getElementById("contentFrame").setAttribute("src", "game.html")
+    document.getElementById("menuGame").style = "color: black; background-color: white;"
+    document.getElementById("menuJukebox").style = "color: white; background-color: #298b5;"
+    document.getElementById("menuHome").style = "color: white; background-color: #298b5;"
+}
